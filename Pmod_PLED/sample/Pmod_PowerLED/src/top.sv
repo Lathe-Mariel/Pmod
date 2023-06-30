aeef8d2a6138aba7596cb56735dd92b1f45e8ede
Lathe <nagai@cqpub.co.jp>
2023-06-30 16:15:26 +0900

    630-2

--- Pmod_PLED/sample/Pmod_PowerLED/src/top.sv
+++ Pmod_PLED/sample/Pmod_PowerLED/src/top.sv
@@ -15,40 +15,45 @@ logic overflow;
 //reg printf;
 
 timer1 timer_instance(clk, overflow);
-reg[7:0] pwm_counter;
-reg[7:0] max=8'd128;
+reg[8:0] pwm_counter;
+reg[8:0] max='d256;
+reg[7:0] change_pwm=0;
 reg inc = 0;
 
 always @(posedge overflow)begin
-    if(pwm_counter == max)begin
+    if(pwm_counter >= max)begin
         red <= 1'd1;
         green <= 1'd1;
         blue <= 1'd1;
-        pwm_counter <= 8'd0;
-        if(inc == 0)begin
-            max = max - 8'd1;
-            if(max == 8'd0)begin
-                inc <= 1;
-            end
-        end else begin
-            max = max + 8'd1;
-            if(max == 8'd128)begin
-                inc <= -1;
-            end
-        end
+        pwm_counter <= 'd0;
+
     end else begin
         red <= 1'd0;
         green <= 1'd0;
         blue <= 1'd0;
         pwm_counter <= pwm_counter + 'd1;
     end
+    if(change_pwm == 0)begin
+        if(inc == 0)begin
+            max <= max - 'd1;
+            if(max <= 'd30)begin
+                inc <= 1;
+            end
+        end else begin
+            max <= max + 'd1;
+            if(max >= 'd400)begin
+                inc <= 0;
+            end
+        end
+    end
+    change_pwm <= change_pwm + 'd1;
 
 end
 
 endmodule
 
 module timer1 #(
-  parameter COUNT_MAX = 2700
+  parameter COUNT_MAX = 1000
 ) (
   input  wire  clk,
   output logic overflow
